f6cc309a265cb00e0539f503042a7634f5ca184c
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-11-14 04:21:36 -0500

    Add placeholder Intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,24 @@
 import "./Intro.scss"
+import { KeyboardArrowDown } from "@material-ui/icons"
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/jb.jpg" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>David Smith</h1>
+                    <h3>Programmer</h3>
+                </div>
+                <a href="#portfolio">
+                    <KeyboardArrowDown className="icon"/>
+                </a>
+            </div>
         </div>
     )
 }
