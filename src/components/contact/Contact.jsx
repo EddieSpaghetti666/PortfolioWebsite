ce326da7d30eba3fe0f8a744d2e3e139f8fa3840
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-11-14 16:52:30 -0500

    Add EmailJs integration. maybe?

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,17 @@
 import "./Contact.scss"
 import { useState } from "react";
+import emailjs from "emailjs-com"
 
 export default function Contact() {
     const [message,setMessage] = useState(false)
     const handleSubmit = (e) =>{
         e.preventDefault();
+        emailjs.sendForm('service_opzlwak', 
+        'template_kwmodap',
+         e.target,
+         'user_T3HDOtQRq2DCTkOqg2NXE').then(res=>{
+             console.log(res);
+         }).catch(err=> console.log(err));
         setMessage(true)
     }
     return (
@@ -15,8 +22,8 @@ export default function Contact() {
             <div className="right">
                 <h2>Contact.</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email"/>
-                    <textarea placeholder="Message"></textarea>
+                    <input type="text" name="user_email" placeholder="Email"/>
+                    <textarea name="message" placeholder="Message"></textarea>
                     <button type="submit">Send</button>
                     {message && <span>Thanks! I'll get back to you ASAP</span>}
                 </form>
