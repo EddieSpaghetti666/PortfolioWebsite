f3a80d338bd7984c8f797ecac2097eee7a1f6e46
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-11-14 03:19:56 -0500

    Implement topbar and hamburger menu animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/Works/Works";
 import Contact from "./components/contact/Contact";
+import { useState } from "react"
 import "./App.scss"
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar open={menuOpen} setOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,29 @@
 import "./Topbar.scss"
+import {Person, Mail} from "@material-ui/icons" 
 
-export default function Topbar() {
+export default function Topbar({open, setOpen}) {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (open && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">A Real Website</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+1 (518)-888-5579</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>Dsmith141996@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setOpen(!open)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
             
         </div>
     )
